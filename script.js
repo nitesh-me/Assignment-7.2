5213a59f85e01c6fd9fbb29da50cf4a1aeb68232
nitesh-me <43075695+nitesh-me@users.noreply.github.com>
2018-09-17 22:41:37 +0530

    Add files via upload
    javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+//using for loop
+console.log('Using for loop');
+for(let i=5;i<=50;i=i+5){
+    console.log(i);
+}
+
+console.log('Using while loop');
+let j=5;
+while(  j<=50){
+   
+    console.log(j);
+    j=j+5;
+}
